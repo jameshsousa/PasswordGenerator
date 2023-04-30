e95dafb5bf58d5bafd0576265a1b54373e4c841b
James Holanda <jameshsousa@gmail.com>
2023-04-30 17:23:30 -0300

    applying javascript on types of character

--- src/script.js
+++ src/script.js
@@ -4,3 +4,18 @@ var $range = document.querySelector('input'),
 $range.addEventListener('input', function() {
   $value.textContent = this.value;
 });
+
+const uppercase = document.getElementById("uppercase").checked;
+const lowercase = document.getElementById("lowercase").checked;
+const number = document.getElementById("number").checked;
+const symbols = document.getElementById("symbols").checked;
+
+var checkbox = document.querySelector("input[name=checkbox]");
+
+function myFunction() {
+  if (uppercase == true && lowercase == true && number == true && symbols == true){
+    alert("Stronger");
+  }
+}
+
+
