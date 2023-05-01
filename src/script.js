026130674da64660699f8248f7175b38980141e8
James Holanda <jameshsousa@gmail.com>
2023-05-01 19:33:23 -0300

    Adding javascript in order to change the display strength

--- src/script.js
+++ src/script.js
@@ -5,17 +5,49 @@ $range.addEventListener('input', function() {
   $value.textContent = this.value;
 });
 
-const uppercase = document.getElementById("uppercase").checked;
-const lowercase = document.getElementById("lowercase").checked;
-const number = document.getElementById("number").checked;
-const symbols = document.getElementById("symbols").checked;
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const numbers = document.getElementById("numbers");
+const symbols = document.getElementById("symbols");
+const strength = document.getElementById("strength");
 
 var checkbox = document.querySelector("input[name=checkbox]");
 
 function myFunction() {
-  if (uppercase == true && lowercase == true && number == true && symbols == true){
-    alert("Stronger");
+  if (uppercase.checked == true && lowercase.checked == true && numbers.checked == true && symbols.checked == true ){
+    strength.innerHTML = "strong";
   }
+  else if   (uppercase.checked == true && lowercase.checked == true && numbers.checked == true || uppercase.checked == true && lowercase.checked == true && symbols.checked == true ){
+    strength.innerHTML = "medium"; 
+  }
+
+  else if(uppercase.checked == true && lowercase.checked == true){
+    strength.innerHTML = "weak";
+  }
+
+  else if  (uppercase.checked == true){
+    strength.innerHTML = "too weak!";
+  }
+  
+  else if  (lowercase.checked == true){
+    strength.innerHTML = "too weak!";
+  } 
+    
 }
+
+const lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','z'];
+const upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','Y','Z'];
+const number = [0,1,2,3,4,5,6,7,8,9];
+const symbol = ['!', '@', '#', '$', '%', '&', '§', '?'];
+
+const u = Math.ceil(Math.random() * upper.length);
+const l = Math.ceil(Math.random() * lower.length);
+const n = Math.ceil(Math.random() * number.length);
+const s = Math.ceil(Math.random() * symbol.length);
+
+
+
+
+
 
 
